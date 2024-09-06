80b78be64af86a70c94521786a831b244504ab17
kasparTPT <115221723+kasparTPT@users.noreply.github.com>
2024-09-06 21:10:28 +0300

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const AddTask = ({ updateTaskList }) => {  // Receiving updateTaskList as a prop
+const AddTask = ({ updateTaskList }) => {
   const [description, setDescription] = useState('');
 
-  const handleSubmit = async (event) => {  // Modified handleSubmit function
+  const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       await axios.post('http://demo2.z-bit.ee/api/tasks', { description }, {
         headers: { Authorization: `Bearer ${localStorage.getItem('userToken')}` }
       });
-      setDescription('');  // Clear the input field
+      setDescription('');
       if (updateTaskList) {
-        updateTaskList();  // Call the passed function to update the list
+        updateTaskList();
       }
     } catch (error) {
       console.error('Error adding task:', error);
